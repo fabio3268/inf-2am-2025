d9b0936d02847d0a9863b2439037ff54d2dab1c4
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-06-20 10:46:56 -0300

    Exercício da Prova

---
+++ JavaScript/05-Armazenamento-local/exemplo01/assets/recupera.js
@@ -0,0 +1,6 @@
+console.log("oi - Recuperação de dados do localStorage");
+
+console.log(localStorage.getItem("name"));
+console.log(localStorage.getItem("age"));
+
+console.log(localStorage.getItem("students"));
---
+++ JavaScript/05-Armazenamento-local/exemplo01/assets/script.js
@@ -0,0 +1,26 @@
+console.log("oi");
+
+localStorage.setItem("name", "João");
+localStorage.setItem("age", 23);
+
+console.log(localStorage.getItem("name"));
+console.log(localStorage.getItem("age"));
+
+let students = [
+    {
+        name: "Fábio",
+        grade: 10
+    },
+    {
+        name: "Godofredo",
+        grade: 7
+    },
+    {
+        name: "Maria",
+        grade: 8
+    }
+];
+//const names = ["João", "Fábio", "Godofredo", "Maria"];
+// JSON
+console.log(JSON.stringify(students, null, 2));
+localStorage.setItem("students", JSON.stringify(students, null, 2));
